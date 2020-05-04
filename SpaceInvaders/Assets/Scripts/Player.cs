bfac2dd64a41f6e6e997028088ac72e12c623ed7
VoidReaper7074 <60455567+VoidReaper7074@users.noreply.github.com>
2020-05-04 15:09:53 -0700

    Updated

---
+++ SpaceInvaders/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float speed = 30;
+
+	// Holds a reference to Bullet objects
+	public GameObject theBullet;
+
+	void FixedUpdate()
+	{
+		float horzMove = Input.GetAxisRaw("Horizontal");
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(horzMove, 0) * speed;
+
+	}
+
+	
+	void Update()
+	{
+
+		// GetButtonDown will only fire once per click 
+		// Jump is assigned to the Space Bar
+		if (Input.GetButtonDown("Jump"))
+		{
+
+			// Create a Bullet at transform.position which
+			// is the ships current location
+			// Quaternion.identity adds Bullet with no rotation
+			Instantiate(theBullet, transform.position, Quaternion.identity);
+
+			// Play bullet fire sound
+			SoundManger.Instance.PlayOneShot(SoundManger.Instance.bulletFire);
+		}
+
+	}
+}
---
+++ SpaceInvaders/Assets/Scripts/SoundManger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManger : MonoBehaviour
+{
+    // Start is 9called before the first frame update
+    public static SoundManger Instance = null;
+
+    public AudioClip alienBuzz1;
+    public AudioClip alienBuzz2;
+    public AudioClip alienDies;
+    public AudioClip bulletFire;
+    public AudioClip shipExplosion;
+
+    private AudioSource soundEffectAudio;
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if( Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource theSource = GetComponent<AudioSource>();
+        soundEffectAudio = theSource;
+        
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+}
